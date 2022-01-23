be34527d1407b5b71e9ef7b67ec1510c97fee967
Phantakan Muangbua <phantakanmpete@gmail.com>
2022-01-23 22:31:01 +0700

    See all page fix

--- lib/pages/seeall_page.dart
+++ lib/pages/seeall_page.dart
@@ -19,86 +19,89 @@ class _SeeallPageState extends State<SeeallPage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: Row(
-                children: const [
-                  Text("Trending Forum",
-                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 22),
-                  ),
-                  Spacer(),
-                  Text(""),
-                ],
-              ),
-            ),
-            Container(
-              height: double.maxFinite,
-              width: 320,
-              child: ListView.builder(
-                itemCount: 5, // number of item to display
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.all(10),
-                    height: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.lightBlue[100],
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.7),
-                          blurRadius: 5.0,
-                          offset: const Offset(0.0, 5.0),
-                        ),
-                      ],
-                    ),
-                    child: Stack(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(10.0),
+            child: const Text("Forum", style: TextStyle(fontSize: 25),)
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SizedBox(
+                    height: 100,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Positioned(
-                          top: 0,
-                          child: Container(
-                            height: 120,
-                            width: 300,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20),
-                              ),
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage("assets/images/valorant.png") // Forum Image
-                              ),
-                            ),
-                          )
+                        AspectRatio(
+                          aspectRatio: 1.0,
+                          child: Image.asset("assets/images/preetoe.jpg"),
                         ),
-                        Positioned(
-                          top: 140,
-                          left: 20,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                "Jaikere",
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20), // Forum Title
-                              ),
-                              Text(
-                                "Jaikere",
-                                style: TextStyle(fontSize: 15), // Forum Subtitle
-                              ),
-                            ],
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text("Lorem Ipsu Lorem Ipsu Lorem Ipsu",
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Padding(padding: EdgeInsets.only(bottom: 2.0)),
+                                      Text("Meanning of Halloween",
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: const [
+                                      Text("Kazuya Jaikere",
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          //color: Colors.black,
+                                        ),
+                                      ),
+                                      Text("21/21/2121",
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          //color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
+                        ),
                       ],
                     ),
-                  );
-                }
-              ),
+                  ),
+                );
+              }, 
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
